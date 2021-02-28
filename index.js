862b36359d0253d752dd84499faf2485f4769b0f
ytanaka <y.matsuura000@gmail.com>
2021-02-28 22:39:07 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const button = document.getElementById('clear');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = "#000000";
+ctx.lineJoin = "round";
+ctx.lineWidth = 5;
+let pathList = [];
+let isDraw = false;
+
+const start = () => {
+  isDraw = true;
+  pathList.push([])
+}
+const move = (event) => {
+  const x = event.clientX - event.target.getBoundingClientRect().left;
+  const y = event.clientY - event.target.getBoundingClientRect().top;
+  if (isDraw) {
+    pathList[pathList.length-1].push({x,y})
+    console.log(pathList);
+  }
+}
+const end = () => {
+  isDraw = false;
+}
+
+canvas.addEventListener('mousedown', start, false);
+canvas.addEventListener('mousemove', move, false);
+canvas.addEventListener('mouseup', end, false);
+button.addEventListener('click', () => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  pathList = [];
+});
+
+const draw = () => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  for (let i = 0;i < pathList.length;i++) {
+    const path = pathList[i];
+    if (path.length < 2) {
+      break;
+    }
+    for(let j = 0;j < path.length-1;j++) {
+      ctx.beginPath();
+      ctx.moveTo(path[j].x, path[j].y);
+      ctx.lineTo(path[j+1].x, path[j+1].y);
+      ctx.closePath();
+      ctx.stroke();
+    }
+  }
+  window.requestAnimationFrame(draw);
+}
+window.requestAnimationFrame(draw);
